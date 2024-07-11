0d73dc4f80996c08d5da0214429836778da52b40
“Chris <chris@floatbit.com>
2024-07-11 16:35:39 -0400

    taxonomies

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php get_header(); ?>
 
 <?php while (have_posts()) : the_post(); ?>
-    <div class="container container-fluid">
-        <?php get_template_part('parts/map'); ?>
-    </div>
+    
+    <?php get_template_part('parts/map'); ?>
+
     <div class="payload-content hidden">
         <h1><?php the_title();?></h1>
         <?php the_content(); ?>
@@ -16,16 +16,17 @@
           </li>
         </ul>
     </div>
+
 <?php endwhile; ?>
 
-<div class="panel panel-page fixed top-0 left-0 h-full w-full overflow-auto hidden">
+<div class="panel panel-page hidden">
     <button class="close absolute">&times;</button>
     <div class="container py-12 payload">
         <!--- payload --->
     </div>
 </div>
 
-<div class="panel panel-image fixed top-0 left-0 h-full w-full overflow-auto hidden">
+<div class="panel panel-image hidden">
     <button class="close absolute">&times;</button>
     <div class="container container-fluid pt-12 pb-6 h-full payload">
         <div class="flex flex-col h-full">
@@ -35,6 +36,59 @@
                 <!--caption-->
             </div>
         </div>
+    </div>
+</div>
+
+<div class="panel panel-explore hidden">
+    <div class="container">
+        <h2>Explore</h2>
+        <form action="#">
+            <div class="flex gap-4 cols-2">
+                <div class="shrink mr-[60px]">
+                    <h3>Category</h3>
+                    <div class="checkboxes">
+                        <?php
+                        $project_categories = get_terms(array(
+                            'taxonomy' => 'project-category',
+                            'hide_empty' => true, // Only get terms that have posts associated with them
+                        ));
+
+                        if (!empty($project_categories) && !is_wp_error($project_categories)) {
+                            foreach ($project_categories as $category) {
+                                echo '<div class="checkbox">';
+                                echo '<input type="checkbox" value="' . esc_attr($category->term_id) . '" id="project-category-' . esc_attr($category->term_id) . '" name="category-' . esc_attr($category->term_id) . '">';
+                                echo '<label for="project-category-' . esc_attr($category->term_id) . '">' . esc_html($category->name) . '</label>';
+                                echo '</div>';
+                            }
+                        }
+                        ?>
+                    </div>
+                </div>
+                <div class="shrink">
+                    <h3>Year</h3>
+                    <div class="checkboxes">
+                        <?php
+                        $project_years = get_terms(array(
+                            'taxonomy' => 'project-year',
+                            'hide_empty' => true, // Only get terms that have posts associated with them
+                        ));
+
+                        if (!empty($project_years) && !is_wp_error($project_years)) {
+                            foreach ($project_years as $year) {
+                                echo '<div class="checkbox">';
+                                echo '<input type="checkbox" value="' . esc_attr($year->term_id) . '" id="project-year-' . esc_attr($year->term_id) . '" name="year-' . esc_attr($year->term_id) . '">';
+                                echo '<label for="project-year-' . esc_attr($year->term_id) . '">' . esc_html($year->name) . '</label>';
+                                echo '</div>';
+                            }
+                        }
+                        ?>
+                    </div>
+                </div>
+            </div>
+            <div class="flex gap-4 cols-2">
+                <input type="submit" value="Filter" class="btn btn-primary">
+            </div>
+        </form>
     </div>
 </div>
 
