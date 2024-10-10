8633af8a961c582a94aa443cb3d8f5b23825f9a2
“Chris <chris@floatbit.com>
2024-10-10 17:18:54 -0400

    box chat

--- footer.php
+++ footer.php
@@ -2,6 +2,7 @@
 
     <?php wp_footer(); ?>
 
+    <?php if ($_GET['box']) : ?>
     <div id="allybox-embed">
         <h3>Strucutral Systems Search</h3>
         <div id="allybox-chat-container"></div>
@@ -14,7 +15,7 @@
             });
         });
     </script>
-
+    <?php endif; ?>
 </body>
 
 </html>
