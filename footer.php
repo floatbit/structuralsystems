a965e16df6eafd25ef78eb7a48eb9f0fc9d6cc3d
“Chris <chris@floatbit.com>
2024-10-11 08:38:45 -0400

    title fix

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
 
     <?php if ($_GET['box']) : ?>
     <div id="allybox-embed">
-        <h3>Strucutral Systems Search</h3>
+        <h3>Structural Systems Search</h3>
         <div id="allybox-chat-container"></div>
     </div>
     <script src="https://dashboard.allybox.app/embed/thread.js"></script>
